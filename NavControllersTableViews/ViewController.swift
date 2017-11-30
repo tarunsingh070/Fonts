5653b0756c4c452511fa321db74a6aa4b456a581
tarunsingh070 <31713053+tarunsingh070@users.noreply.github.com>
2017-11-29 19:12:36 -0500

    Initial Commit

---
+++ NavControllersTableViews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NavControllersTableViews
+//
+//  Created by Tarun Singh on 2017-11-29.
+//  Copyright © 2017 Tarun Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
